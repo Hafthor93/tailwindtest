feb4364ea12c53ba6b06434a357fee3ac0d619ba
Hafthor93 <101139553+Hafthor93@users.noreply.github.com>
2022-09-28 22:55:18 +0000

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    " ./src/**/*.{html,js}"],
+  theme: { 
+    colors: {
+      "siminnpay": "#00aef3",
+      "white": "white",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
